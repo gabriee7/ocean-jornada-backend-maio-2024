25136e7e5a46de49e9c16ecbdefbcb593a819187
Gabriel Souza <102442477+gabriee7@users.noreply.github.com>
2024-05-14 17:12:40 +0000

    Implementação endpoint Read All

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World!')
 })
 
 app.get('/oi', function (req, res) {
   res.send('Olá, mundo!')
+})
+
+
+const itens = [
+              'Rick Sanchez', 
+              'Morty Smith', 
+              'Summer Smith'
+              ]
+
+//Endpoint Read All [GET] /item
+app.get('/item', function (req, res){
+  res.send(itens)
 })
 
 app.listen(3000)
