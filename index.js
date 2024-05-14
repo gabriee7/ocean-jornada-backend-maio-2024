6657c2bae9311dfd24bbaba11bbcb8cd6e4a8e97
Gabriel Souza <102442477+gabriee7@users.noreply.github.com>
2024-05-14 18:42:46 +0000

    Implementacao endpoint Delete by ID

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/item', function(req, res) {
 })
 
 
-//Endpoint Update [PUT] /item/:id
+//Endpoint Update by ID [PUT] /item/:id
 app.put('/item/:id', function(req, res) {
   //Acessa :id da requisicao 
   const id = req.params.id
@@ -54,6 +54,15 @@ app.put('/item/:id', function(req, res) {
   itens[id] = updateItem
 
   res.send(`Item id ${id} atualizado para ${updateItem}.`)
+})
+
+//Endpoint Delete by ID [DELETE] /item/:id
+app.delete('/item/:id', function(req, res) {
+  const id = req.params.id
+
+  delete itens[id]
+
+  res.send(`ID: ${id} removido!`)
 })
 
 app.listen(3000)
